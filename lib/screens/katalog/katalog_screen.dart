3086c17d54e575515c03cb926216298a42599521
milde <milde@hs-fulda.de>
2021-02-19 12:10:46 +0100

    KatalogScreen eingeführt

---
+++ lib/screens/katalog/katalog_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KatalogScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Katalog"),
+      ),
+      body: Container(),
+    );
+  }
+}
